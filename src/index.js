b37a77926cfd953cd037b2598ac1d4ff26b2be74
ecekoprucu <ecekoprucu@gmail.com>
2023-05-07 16:42:06 +0300

    vercel deploy remove (1st)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import AppWrapper from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppWrapper />
   </React.StrictMode>,
   document.getElementById("root"),
 );
